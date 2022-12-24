7d258d4f1c69a9b690194632fe9d6a51e56eb388
Carlos Eduardo <caarloseduardo.engcivil@gmail.com>
2022-12-24 00:28:51 -0300

    Final

--- AnaliseDeDados/main.py
+++ AnaliseDeDados/main.py
@@ -1,13 +1,10 @@
 import pandas as pd
 from twilio.rest import Client
 
-# Your Account SID from twilio.com/console
 account_sid = "AC2296c00a2e4265232a4ee4d989cb7ef8"
-# Your Auth Token from twilio.com/console
 auth_token  = "4a0aab32281011dd0fedb7f683e251b4"
 client = Client(account_sid, auth_token)
 
-# ABRIR OS 6 ARQUIOS EM EXCEL
 lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
 for mes in lista_meses:
@@ -21,12 +18,3 @@ for mes in lista_meses:
             from_="+12187890698",
             body=f'No mês {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}'    )
         print(message.sid)
-
-# PARA CADA ARQUIVO:
-
-
-# VERIFICAR SE ALGUM VALOR NA COLUNA VENDAS DAQUELE ARQUIVO É MAIOR QUE 55.000
-
-# SE FOR MAIOR DO QUE 55.000 -> ENVIA UM SMS COM O NOME, O MÊS E AS VENDAS DO VENDEDOR
-
-# CASO NÃO SEJA MAIOR DO QUE 55.000 NÃO QUERO FAZER NADA
